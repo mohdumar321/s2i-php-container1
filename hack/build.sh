779f959141715a2fed7e9bd7dd486c1086ea07a5
SonamVerma9 <sonam.verma@atos.net>
2017-02-09 14:07:24 +0530

    Update build.sh

--- hack/build.sh
+++ hack/build.sh
@@ -49,7 +49,16 @@ function docker_build_with_version {
 function squash {
   # FIXME: We have to use the exact versions here to avoid Docker client
   #        compatibility issues
-  easy_install --user docker_py==2.0.1 docker-squash==1.0.5
+  
+  
+   #########easy_install -q --user docker_py==1.7.2 docker-squash==1.0.4
+   
+   
+   easy_install -q --user https://github.com/goldmann/docker-squash/archive/master.zip
+   easy_install -q --user https://github.com/docker/docker-py/archive/master.zip
+   
+   
+   
   base=$(awk '/^FROM/{print $2}' $1)
   ${HOME}/.local/bin/docker-squash -f $base ${IMAGE_NAME}
 }
