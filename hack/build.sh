01d8c40a8a9ffd258a33c2036903d7945ebeddff
SonamVerma9 <sonam.verma@atos.net>
2017-02-09 14:21:37 +0530

    Update build.sh

--- hack/build.sh
+++ hack/build.sh
@@ -52,7 +52,7 @@ function squash {
   
   
    #########easy_install -q --user docker_py==1.7.2 docker-squash==1.0.4
-   pip -q --user docker_py==1.7.2 docker-squash==1.0.4
+   pip -q --user docker_py==2.0.2  docker-squash==1.0.5
    
  #  easy_install -q --user https://github.com/SonamVerma9/s2i-php-container.git docker_py==2.0.2 docker-squash==1.0.5
    
